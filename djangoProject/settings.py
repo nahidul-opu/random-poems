db2ca7f98ab11b2c5171b2d984ca17a886b39d14
Md. Nahidul Islam Opu <37839072+nahidul-opu@users.noreply.github.com>
2022-02-14 01:31:27 +0600

    static files

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
