fec4fbdb79b04d26c2fcc3dffdb0a6fe80a07845
Md. Nahidul Islam Opu <37839072+nahidul-opu@users.noreply.github.com>
2022-02-14 01:56:12 +0600

    Update wsgi.py

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-from whitenoise.django import DjangoWhiteNoise
-from django.core.wsgi import get_wsgi_application
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
